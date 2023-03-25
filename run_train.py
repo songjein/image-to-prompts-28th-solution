cf7585b3244f24f0bbd6e80797a5d5d7994e2243
Jein Song <jein@scatterlab.co.kr>
2023-03-25 16:49:47 +0000

    refactor: 스크립트 이름 변경
    Signed-off-by: Jein Song <jein@scatterlab.co.kr>

--- run_train.py
+++ run_train.py
@@ -1,22 +1,18 @@
 import os
 import random
-import warnings
+import sys
 
 import numpy as np
-import pandas as pd
-import timm
 import torch
-from scipy import spatial
-from sklearn.model_selection import train_test_split
+from datasets import load_dataset
 from timm.utils import AverageMeter
-from torch import nn
-from torch.nn.utils import clip_grad_norm_
-from torch.optim.lr_scheduler import CosineAnnealingLR
+from torch.utils.data import DataLoader, Dataset
 from tqdm import tqdm
+from transformers import (AutoModelForCausalLM, AutoProcessor,
+                          BlipForConditionalGeneration,
+                          get_cosine_schedule_with_warmup)
 
-from dataset import get_dataloaders
-
-warnings.filterwarnings("ignore")
+import wandb
 
 
 def seed_everything(seed):
@@ -30,123 +26,152 @@ def seed_everything(seed):
         torch.backends.cudnn.deterministic = True
 
 
-def cosine_similarity(y_trues, y_preds):
-    return np.mean(
-        [
-            1 - spatial.distance.cosine(y_true, y_pred)
-            for y_true, y_pred in zip(y_trues, y_preds)
-        ]
-    )
+class ImageCaptioningDataset(Dataset):
+    def __init__(self, dataset, processor):
+        self.dataset = dataset
+        self.processor = processor
 
+    def __len__(self):
+        return len(self.dataset)
 
-def train(train_df, valid_df, model_name, input_size, batch_size, num_epochs, lr):
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    dataloaders = get_dataloaders(train_df, valid_df, input_size, batch_size)
+    def __getitem__(self, idx):
+        item = self.dataset[idx]
 
-    model = timm.create_model(model_name, pretrained=True, num_classes=384)
-    model.set_grad_checkpointing()
-    model.to(device)
-
-    optimizer = torch.optim.AdamW(model.parameters(), lr=lr)
-
-    ttl_iters = num_epochs * len(dataloaders["train"])
-    scheduler = CosineAnnealingLR(optimizer, T_max=ttl_iters, eta_min=1e-6)
-    criterion = nn.CosineEmbeddingLoss()
-
-    best_score = -1.0
-
-    for epoch in range(num_epochs):
-        train_meters = {
-            "loss": AverageMeter(),
-            "cos": AverageMeter(),
-        }
-        model.train()
-        for X, y in tqdm(dataloaders["train"]):
-            X, y = X.to(device), y.to(device)
-
-            optimizer.zero_grad()
-
-            X_out = model(X)
-            target = torch.ones(X.size(0)).to(device)
-            loss = criterion(X_out, y, target)
-            loss.backward()
-
-            optimizer.step()
-            scheduler.step()
-
-            trn_loss = loss.item()
-            trn_cos = cosine_similarity(
-                X_out.detach().cpu().numpy(), y.detach().cpu().numpy()
-            )
-
-            train_meters["loss"].update(trn_loss, n=X.size(0))
-            train_meters["cos"].update(trn_cos, n=X.size(0))
-
-        print(
-            "Epoch {:d} / trn/loss={:.4f}, trn/cos={:.4f}".format(
-                epoch + 1, train_meters["loss"].avg, train_meters["cos"].avg
-            )
+        encoding = self.processor(
+            images=item["image"],
+            text=item["text"],
+            padding="max_length",
+            return_tensors="pt",
         )
 
-        val_meters = {
-            "loss": AverageMeter(),
-            "cos": AverageMeter(),
-        }
-        model.eval()
-        for X, y in tqdm(dataloaders["val"]):
-            X, y = X.to(device), y.to(device)
+        # remove batch dimension
+        encoding = {k: v.squeeze() for k, v in encoding.items()}
 
-            with torch.no_grad():
-                X_out = model(X)
-                target = torch.ones(X.size(0)).to(device)
-                loss = criterion(X_out, y, target)
+        return encoding
 
-                val_loss = loss.item()
-                val_cos = cosine_similarity(
-                    X_out.detach().cpu().numpy(), y.detach().cpu().numpy()
-                )
 
-            val_meters["loss"].update(val_loss, n=X.size(0))
-            val_meters["cos"].update(val_cos, n=X.size(0))
+def evaluate(valid_dataloader, model):
+    model.eval()
+    data_loader_tqdm = tqdm(valid_dataloader, file=sys.stdout)
 
-        print(
-            "Epoch {:d} / val/loss={:.4f}, val/cos={:.4f}".format(
-                epoch + 1, val_meters["loss"].avg, val_meters["cos"].avg
+    val_meters = {
+        "loss": AverageMeter(),
+    }
+
+    with torch.no_grad():
+        for idx, batch in enumerate(data_loader_tqdm):
+            input_ids = batch.pop("input_ids").to(device)
+            pixel_values = batch.pop("pixel_values").to(device)
+
+            outputs = model(
+                input_ids=input_ids, pixel_values=pixel_values, labels=input_ids
             )
-        )
+            loss = outputs.loss
 
-        if val_meters["cos"].avg > best_score:
-            best_score = val_meters["cos"].avg
-            torch.save(model.state_dict(), f"outputs_{num_epochs}ep/{model_name}.pth")
+            val_meters["loss"].update(loss.item(), n=input_ids.size(0))
+
+            data_loader_tqdm.set_description(f"Epoch {epoch}, loss: {loss.item()}")
+
+    model.train()
+
+    return val_meters["loss"].avg
 
 
 if __name__ == "__main__":
+    # CUDA_VISIBLE_DEVICES=0,1 python run_train.py
 
-    class CFG:
-        model_name = "vit_base_patch16_224"
-        input_size = 224
-        batch_size = 64
-        num_epochs = 3
-        lr = 1e-4
-        seed = 42
+    wandb.login()
 
-    seed_everything(CFG.seed)
+    model_name = "Salesforce/blip-image-captioning-base"
+    # model_name = "microsoft/git-base"
+    epochs = 3
+    batch_size = 16
+    valid_batch_size = 16
+    learning_rate = 5e-5
+    valid_steps = 1000
+    warmup_ratio = 0.05
+    seed = 42
+    memo = f"git-model-{seed}s-{epochs}ep-{model_name}"
 
-    df = pd.read_csv("./data/diffusiondb.csv")
-    for index, row in df.iterrows():
-        df.at[index, "filepath"] = df.at[index, "filepath"].replace(
-            "/kaggle/input/", "./data/"
-        )
-
-    train_df, valid_df = train_test_split(df, test_size=0.1, random_state=CFG.seed)
-
-    os.makedirs(f"outputs_{CFG.num_epochs}ep", exist_ok=True)
-    train(
-        train_df,
-        valid_df,
-        CFG.model_name,
-        CFG.input_size,
-        CFG.batch_size,
-        CFG.num_epochs,
-        CFG.lr,
+    wandb.init(
+        name=memo,
+        project="prompts-to-image",
+        config={
+            "epochs": epochs,
+            "model_name": model_name,
+            "batch_size": batch_size,
+            "learning_rate": learning_rate,
+            "valid_batch_size": valid_batch_size,
+            "warmup_ratio": warmup_ratio,
+            "seed": seed,
+        },
     )
+
+    output_path = f"output_{memo}"
+    os.makedirs(output_path, exist_ok=True)
+
+    seed_everything(seed)
+
+    train_dataset = load_dataset("imagefolder", data_dir="./diffusion", split="train")
+    valid_dataset = load_dataset(
+        "imagefolder", data_dir="./diffusion", split="validation"
+    )
+
+    processor = AutoProcessor.from_pretrained(model_name)
+
+    train_dataset = ImageCaptioningDataset(train_dataset, processor)
+    valid_dataset = ImageCaptioningDataset(valid_dataset, processor)
+    # input_ids [512], attention_mask [512], pixel_values [3, 224, 224]
+
+    train_dataloader = DataLoader(train_dataset, shuffle=True, batch_size=batch_size)
+    valid_dataloader = DataLoader(valid_dataset, shuffle=True, batch_size=batch_size)
+
+    if "blip" in model_name:
+        model = BlipForConditionalGeneration.from_pretrained(model_name)
+    else:
+        model = AutoModelForCausalLM.from_pretrained(model_name)
+    optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model.to(device)
+
+    total_steps = len(train_dataloader)
+    warmup_steps = int(total_steps * warmup_ratio)
+    scheduler = get_cosine_schedule_with_warmup(optimizer, warmup_steps, total_steps)
+
+    model.train()
+
+    best_score = 9999
+    for epoch in range(epochs):
+        data_loader_tqdm = tqdm(train_dataloader, file=sys.stdout)
+        for idx, batch in enumerate(data_loader_tqdm):
+            input_ids = batch.pop("input_ids").to(device)
+            pixel_values = batch.pop("pixel_values").to(device)
+
+            outputs = model(
+                input_ids=input_ids, pixel_values=pixel_values, labels=input_ids
+            )
+            loss = outputs.loss
+
+            wandb.log({"train_loss": loss.item()})
+            data_loader_tqdm.set_description(f"Epoch {epoch}, loss: {loss.item()}")
+
+            loss.backward()
+            optimizer.step()
+            scheduler.step()
+            optimizer.zero_grad()
+
+            if idx > 0 and idx % valid_steps == 0:
+                valid_score = evaluate(valid_dataloader, model)
+                wandb.log({"valid_loss": valid_score})
+
+                if valid_score < best_score:
+                    best_score = valid_score
+
+                    torch.save(
+                        model.state_dict(),
+                        os.path.join(output_path, f"best_model_ep_{epoch}.pth"),
+                    )
+
+        torch.save(
+            model.state_dict(), os.path.join(output_path, f"last_model_ep_{epoch}.pth")
+        )

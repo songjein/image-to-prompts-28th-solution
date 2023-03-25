dccd4165edce9f82bf299f49303d43d91e903666
Jein Song <jeinsong100@gmail.com>
2023-03-25 06:43:33 +0000

    feat: captions.jsonl 파일 생성 스크립트
    Signed-off-by: Jein Song <jeinsong100@gmail.com>

---
+++ make_captions.py
@@ -0,0 +1,22 @@
+import json
+import os
+
+if __name__ == "__main__":
+    captions = []
+    with open("prompts-large.txt") as f:
+        for idx, line in enumerate(f):
+            file_name = f"{idx:08d}.jpg"
+            path = os.path.join("./images/", file_name)
+            if not os.path.exists(path):
+                continue
+            prompt = line.strip()
+            captions.append(
+                {
+                    "file_name": file_name,
+                    "text": prompt,
+                }
+            )
+
+    with open("./captions.jsonl", "w", encoding="utf-8") as f:
+        for caption in captions:
+            f.write(json.dumps(caption, ensure_ascii=False) + "\n")

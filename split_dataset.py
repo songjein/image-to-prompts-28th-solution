7b5df61d510d078660ca83cbc81d68c4599a6d06
Jein Song <jein@scatterlab.co.kr>
2023-03-27 02:08:26 +0000

    refactor: 추론 스크립트 및 데이터 스플릿 변경
    Signed-off-by: Jein Song <jein@scatterlab.co.kr>

--- split_dataset.py
+++ split_dataset.py
@@ -21,25 +21,21 @@ if __name__ == "__main__":
     os.makedirs("./diffusion/validation/", exist_ok=True)
 
     for caption in train_captions:
-        _from = os.path.join(root, caption["file_name"].replace("/images/", ""))
-        _to = os.path.join(
-            "./diffusion/train", caption["file_name"].replace("/images/", "")
-        )
+        _from = os.path.join(root, caption["file_name"])
+        _to = os.path.join("./diffusion/train", caption["file_name"])
         shutil.move(_from, _to)
 
     for caption in valid_captions:
-        _from = os.path.join(root, caption["file_name"].replace("/images/", ""))
-        _to = os.path.join(
-            "./diffusion/validation", caption["file_name"].replace("/images/", "")
-        )
+        _from = os.path.join(root, caption["file_name"])
+        _to = os.path.join("./diffusion/validation", caption["file_name"])
         shutil.move(_from, _to)
 
     with open("./diffusion/train/metadata.jsonl", "w") as f:
         for caption in train_captions:
-            caption["file_name"] = caption["file_name"].replace("/images/", "")
+            caption["file_name"] = caption["file_name"]
             f.write(json.dumps(caption, ensure_ascii=False) + "\n")
 
     with open("./diffusion/validation/metadata.jsonl", "w") as f:
         for caption in valid_captions:
-            caption["file_name"] = caption["file_name"].replace("/images/", "")
+            caption["file_name"] = caption["file_name"]
             f.write(json.dumps(caption, ensure_ascii=False) + "\n")

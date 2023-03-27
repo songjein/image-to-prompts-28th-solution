883fbd8dad7f511e85d92e061a513575b4682079
Jein Song <jein@scatterlab.co.kr>
2023-03-27 10:02:09 +0000

    refactor: split dataset 중복 체크 및 분포 확인
    Signed-off-by: Jein Song <jein@scatterlab.co.kr>

--- split_dataset.py
+++ split_dataset.py
@@ -5,6 +5,7 @@ import shutil
 
 if __name__ == "__main__":
     root = "./diffusion/images/"
+    move_file = False
 
     captions = []
     with open("./diffusion/captions.jsonl") as f:
@@ -20,22 +21,42 @@ if __name__ == "__main__":
     os.makedirs("./diffusion/train/", exist_ok=True)
     os.makedirs("./diffusion/validation/", exist_ok=True)
 
+    visited = set()
+    tlens = []
     for caption in train_captions:
+        visited.add(caption["text"].strip())
+        tlens.append(len(caption["text"].strip()))
         _from = os.path.join(root, caption["file_name"])
         _to = os.path.join("./diffusion/train", caption["file_name"])
-        shutil.move(_from, _to)
+        if move_file:
+            shutil.move(_from, _to)
 
+    dup_cnt = 0
+    vlens = []
     for caption in valid_captions:
+        if caption["text"] in visited:
+            print(caption)
+            dup_cnt += 1
+            continue
+        vlens.append(len(caption["text"].strip()))
         _from = os.path.join(root, caption["file_name"])
         _to = os.path.join("./diffusion/validation", caption["file_name"])
-        shutil.move(_from, _to)
+        if move_file:
+            shutil.move(_from, _to)
 
-    with open("./diffusion/train/metadata.jsonl", "w") as f:
-        for caption in train_captions:
-            caption["file_name"] = caption["file_name"]
-            f.write(json.dumps(caption, ensure_ascii=False) + "\n")
+    print(dup_cnt)
+    print("tlens", sum(tlens) / len(tlens))
+    print("vlens", sum(vlens) / len(vlens))
+    print("num train samples", len(tlens))
+    print("num valid samples", len(vlens))
 
-    with open("./diffusion/validation/metadata.jsonl", "w") as f:
-        for caption in valid_captions:
-            caption["file_name"] = caption["file_name"]
-            f.write(json.dumps(caption, ensure_ascii=False) + "\n")
+    if move_file:
+        with open("./diffusion/train/metadata.jsonl", "w") as f:
+            for caption in train_captions:
+                caption["file_name"] = caption["file_name"]
+                f.write(json.dumps(caption, ensure_ascii=False) + "\n")
+
+        with open("./diffusion/validation/metadata.jsonl", "w") as f:
+            for caption in valid_captions:
+                caption["file_name"] = caption["file_name"]
+                f.write(json.dumps(caption, ensure_ascii=False) + "\n")

fdd2d3ccc08d8274da6fe4ceff8de66db8289f86
Jein Song <jein@scatterlab.co.kr>
2023-03-31 14:05:12 +0000

    feat: dedup prompts
    Signed-off-by: Jein Song <jein@scatterlab.co.kr>

--- dedup_prompts.py
+++ dedup_prompts.py
@@ -1,48 +1,49 @@
 import torch
 from sentence_transformers import SentenceTransformer
-from tqdm import tqdm
 
 if __name__ == "__main__":
-    st_model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
+    st_model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2").cuda()
     cosim = torch.nn.CosineSimilarity(dim=1, eps=1e-7)
 
     prompts = []
-    embeds_dict = dict()
-
-    with open("./data/openprompts_filtered_16_256.txt") as f:
-        for idx, line in tqdm(enumerate(f)):
+    with open("./resources/prompts_all.txt") as f:
+        for idx, line in enumerate(f):
             prompts.append(line.strip())
-            embeds_dict[idx] = torch.tensor(st_model.encode([line.strip()]))
 
-    deleted_indices = set()
-    target_index = 0
-    while target_index < len(prompts):
-        if target_index in deleted_indices:
-            target_index += 1
+    embeds = torch.tensor(st_model.encode(prompts)).cuda()
+    print("embeds created")
+
+    deleted_ids = set()
+
+    chunk_size = 1000
+    for i in range(0, embeds.size(0), chunk_size):
+        start = i
+        end = i + chunk_size
+        chunk = embeds[i:end]
+
+        _embeds = embeds[start:end]
+
+        similarities = torch.mm(_embeds, embeds.T)
+        indices = torch.nonzero(similarities > 0.85, as_tuple=True)
+
+        pairs = []
+        for x, y in zip(indices[0].tolist(), indices[1].tolist()):
+            _x = x + start
+            if _x == y:
+                continue
+            pairs.append((_x, y))
+
+        deleted_ids.update(set([p[1] for p in pairs]))
+
+    print("deleted ids:", len(deleted_ids))
+
+    results = []
+    for idx, prompt in enumerate(prompts):
+        if idx in deleted_ids:
             continue
+        else:
+            results.append(prompt + "\n")
 
-        target_emb = embeds_dict[target_index]
-
-        for idx in embeds_dict.keys():
-            if idx == target_index:
-                continue
-
-            if idx in deleted_indices:
-                continue
-
-            comp_emb = embeds_dict[idx]
-            if cosim(target_emb, comp_emb) > 0.75:
-                deleted_indices.add(idx)
-
-                print("target", target_index, "deleted", idx)
-                print("A:", prompts[target_index])
-                print("B:", prompts[idx])
-                print()
-
-        target_index += 1
-
-    with open("./data/openprompts_dedup.txt", "w", encoding="utf-8") as f:
-        for idx, prompt in enumerate(prompts):
-            if idx in deleted_indices:
-                continue
-            f.write(prompt + "\n")
+    with open(f"./resources/prompts_all_dedup_085.txt", "w", encoding="utf-8") as f:
+        for idx, line in enumerate(results):
+            f.write(line)

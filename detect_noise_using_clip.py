a1843f34b6afc14dd3b9d596761a2ff2ea1392be
Jein Song <jein@scatterlab.co.kr>
2023-04-25 14:18:20 +0000

    fix: clip noise remover 버그 수정
    Signed-off-by: Jein Song <jein@scatterlab.co.kr>

--- detect_noise_using_clip.py
+++ detect_noise_using_clip.py
@@ -1,14 +1,12 @@
-
-import os
 import json
+import os
+
 import torch
 from PIL import Image
-from transformers import CLIPProcessor, CLIPModel, CLIPTokenizer
+from transformers import CLIPModel, CLIPProcessor, CLIPTokenizer
 
 if __name__ == "__main__":
-
     model_name = "laion/CLIP-ViT-H-14-laion2B-s32B-b79K"
-    # model_name = "openai/clip-vit-base-patch32"
     model = CLIPModel.from_pretrained(model_name).cuda()
     processor = CLIPProcessor.from_pretrained(model_name)
     tokenizer = CLIPTokenizer.from_pretrained(model_name)
@@ -24,36 +22,37 @@ if __name__ == "__main__":
 
     batch_size = 16
 
-    with open("./resources/dissim_pairs.txt", "w", encoding="utf-8") as f:
-        for i in range(0, len(items), batch_size):
-
+    with open("./resources/dissim_pairs_025_v7_train.txt", "w", encoding="utf-8") as f:
+        for start_index in range(0, len(items), batch_size):
             pixel_values = None
             texts = []
-            for item in items[i: i+batch_size]:
+            for item in items[start_index : start_index + batch_size]:
                 texts.append(item["text"])
                 path = os.path.join(root, item["file_name"])
                 image = Image.open(path)
-                pv = processor(text=None, images=image, return_tensors="pt", padding=True)["pixel_values"]
+                pv = processor(text=None, images=image, return_tensors="pt")[
+                    "pixel_values"
+                ]
 
                 if pixel_values is None:
                     pixel_values = pv
                 else:
                     pixel_values = torch.vstack((pixel_values, pv))
-            
-            pixel_values = pixel_values.cuda()
 
+            pixel_values = pixel_values.cuda()
             image_embeds = model.get_image_features(pixel_values)
 
-            text_inputs = tokenizer(texts, return_tensors="pt", padding=True)
+            text_inputs = tokenizer(
+                texts, return_tensors="pt", max_length=77, truncation=True, padding=True
+            )
             text_inputs = {key: val.cuda() for key, val in text_inputs.items()}
             text_embeds = model.get_text_features(**text_inputs)
 
             scores = cosim(image_embeds, text_embeds)
 
-            for j, score in enumerate(scores):
-                base = batch_size * i
-                index = base + j
-                
+            for in_batch_index, score in enumerate(scores):
+                index = start_index + in_batch_index
+
                 if score < 0.25:
                     score = float(score)
                     item = items[index]
